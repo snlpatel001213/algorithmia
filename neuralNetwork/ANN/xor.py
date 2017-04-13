d7d3528ce1262eefdb2dd4e9fdabf0f41616cc52
snlpatel001213 <snlpatel001213@hotmail.com>
2017-04-13 10:56:32 +0530

    changes made to ANN

--- neuralNetwork/ANN/xor.py
+++ neuralNetwork/ANN/xor.py
@@ -1,6 +1,4 @@
 import math
-import random
-
 """
 defining XOR gate, [x1, x2 , y]
 """
@@ -25,33 +23,47 @@ error = 0
 # defining epochs
 Epochs = 2000
 count = 0
-for j in range(2000):
+
+# run this repeatedly for number of Epochs
+for j in range(Epochs):
     print"squaredError", squaredError
     # initializing squaredError per epoch
     squaredError = 0
     for i in range(4): # iterating through each case for given iteration
+        """
+        calculating output at each perceptron
+        """
         y3 = 1 / (1 + math.exp(-((XOR[i][0] * w13) + (XOR[i][1] * w23))))
         y4 = 1 / (1 + math.exp(-(XOR[i][0] * w14 + XOR[i][1] * w24)))
         y5 = 1 / (1 + math.exp(-(y3 * w35 + y4 * w45)))
+        """
+        calculating error
+        """
         error = XOR[i][2] - y5
 
+        """
+        calculating partial error and change in weight for output and hidden perceptron
+        """
         del5 = y5 * (1 - y5) * error
         dw35 = alpha * y3 * del5
         dw45 = alpha * y4 * del5
         dt5 = alpha * (-1) * del5
 
+        """
+            calculating partial error and change in weight for input and hidden perceptron
+        """
+
         del3 = y3 * (1 - y3) * del5 * w35
         del4 = y4 * (1 - y4) * del5 * w45
-
-        # CHANGES BETWEEN INPUT AND MIDDLE LAYER
         dw13 = alpha * XOR[i][0] * del3
         dw23 = alpha * XOR[i][1] * del3
         dt3 = alpha * (-1) * del3
         dw14 = alpha * XOR[i][0] * del4
         dw24 = alpha * XOR[i][1] * del4
         dt4 = alpha * (-1) * del4
-        # print (">>>>>>>>>>>>>>",del4,del3,del5)
-        # corrections
+        """
+        calculating weight and bias update
+        """
         w13 = w13 + dw13
         w14 = w14 + dw14
         w23 = w23 + dw23
@@ -61,13 +73,23 @@ for j in range(2000):
         t3 = t3 + dt3
         t4 = t4 + dt4
         t5 = t5 + dt5
-        if y5 < 0.5:
-            temporary = 0
-        else:
-            temporary = 1
 
-        # print (">>",XOR[i][0],XOR[i][1],">>",temporary," actual ",XOR[i][2])
-        # print (">>>>>",error*error)
+        """
+        Since y5 will be in float number between (0 - 1)
+        Here we have used 0.5 as threshold, if output is above 0.5 then class will be 1 else 0
+        """
+        if y5 < 0.5:
+            class_ = 0
+        else:
+            class_ = 1
+        """
+        uncomment below line to see predicted and actual output
+        """
+        # print ("Predicted",class_," actual ",XOR[i][2])
+        """
+        calculating squared error
+        """
         squaredError = squaredError + (error * error)
         if squaredError < 0.001:
+            # if error is below   0.001, terminate training (premature termination)
             break
