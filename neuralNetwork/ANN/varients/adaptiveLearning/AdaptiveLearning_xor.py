633925ac3a7ae035ad960d15dcddb80c3046b65e
sunil <snlpatel001213@hotmail.com>
2017-04-15 17:42:44 +0530

    ANn completed

--- neuralNetwork/ANN/varients/adaptiveLearning/AdaptiveLearning_xor.py
+++ neuralNetwork/ANN/varients/adaptiveLearning/AdaptiveLearning_xor.py
@@ -25,7 +25,7 @@ error = 0
 Epochs = 2000
 count = 0
 # run this repeatedly for number of Epochs
-global slopArray
+global slopArray  # to store slops
 slopArray = []
 
 
@@ -55,113 +55,117 @@ def findTrendline(xArray, yArray):
 def changeAdaptively(squaredErrorArray, EpochArray, alpha):
     """
     to change learning rate(alpha) adaptively
-    :param squaredErrorArray:
-    :param EpochArray:
+    :param squaredErrorArray:  Array containing squared error for all completed epochs: [1.2340,0.1,0.45,0.85,0.4,0.4430,0.3244]
+    :param EpochArray: Array containing all completed epochs: [1,2,3,4,5,....50,51.]
+    :param alpha: [current learning rate]
     :return:
     """
     # print "went ti this func"
     # print squaredErrorArray[-10:],EpochArray[-10:]
-    m, b = findTrendline(squaredErrorArray[-10:], EpochArray[-10:])
+    m, b = findTrendline(squaredErrorArray[-10:], EpochArray[-10:]) # find slop for current lat 10 error value
     try:
-        if m > slopArray[-1]:
+        if m > slopArray[-1]: #slopArray[-1] previous slop, m current slop
             """
-                If present slop is less than previous one, it indicates decrease in error gradually
+                If present slop is greater than previous one, it indicates decrease in error gradually
+                This usually happens at beginning and middle of learning process
                 then increase learning rate further to decelerate error further
 
             """
             slopArray.append(m)
-            alpha = alpha * 1.08
+            newAlpha = alpha * 1.08
         else:
             """
-            If present slop is more than previous, it indicates instability or increase in error
+            If present slop is less than previous, it indicates instability or very less chane  in error
+            this usually happens near to convergence point.
             then decrease learning rate
             """
             slopArray.append(m)
-            alpha = alpha / 1.04
-        return alpha
+            newAlpha = alpha / 1.04
+        return newAlpha
     except:
         # for first iteration when nothing will be there in slopArray
         # so slop will throw exception and will be handled by except block
         slopArray.append(m)
         return alpha
 
+def Main():
+    EpochArray = []
+    squaredErrorArray = []
+    for j in range(Epochs):
+        # printing squaredError, alpha after each epoch
+        print"squaredError", squaredError, alpha
+        # making update to learning rate after every 10 epochs
+        if j % 10 == 0 and j != 0:
+            alpha = changeAdaptively(squaredErrorArray, EpochArray, alpha)
+        # appending squared error to squaredErrorArray
+        squaredErrorArray.append(squaredError)
+        # appending number of completed epochs to EpochArray
+        EpochArray.append(j)
+        squaredError = 0
+        for i in range(4):  # iterating through each case for given iteration
 
-EpochArray = []
-squaredErrorArray = []
-for j in range(Epochs):
-    # printing squaredError, alpha after each epoch
-    print"squaredError", squaredError, alpha
+            """
+            calculating output at each perceptron
+            """
+            y3 = 1 / (1 + math.exp(-((XOR[i][0] * w13) + (XOR[i][1] * w23 - t3))))
+            y4 = 1 / (1 + math.exp(-(XOR[i][0] * w14 + XOR[i][1] * w24 - t4)))
+            y5 = 1 / (1 + math.exp(-(y3 * w35 + y4 * w45 - t5)))
+            """
+            calculating error
+            """
+            error = XOR[i][2] - y5
 
-    # making update to learning rate after every 10 epochs
-    if j % 10 == 0 and j != 0:
-        alpha = changeAdaptively(squaredErrorArray, EpochArray, alpha)
-    # appending squared error to squaredErrorArray
-    squaredErrorArray.append(squaredError)
-    # appending number of completed epochs to EpochArray
-    EpochArray.append(j)
-    squaredError = 0
-    for i in range(4):  # iterating through each case for given iteration
+            """
+            calculating partial error and change in weight for output and hidden perceptron
+            """
+            del5 = y5 * (1 - y5) * error
+            dw35 = alpha * y3 * del5
+            dw45 = alpha * y4 * del5
+            dt5 = alpha * (-1) * del5
 
-        """
-        calculating output at each perceptron
-        """
-        y3 = 1 / (1 + math.exp(-((XOR[i][0] * w13) + (XOR[i][1] * w23 - t3))))
-        y4 = 1 / (1 + math.exp(-(XOR[i][0] * w14 + XOR[i][1] * w24 - t4)))
-        y5 = 1 / (1 + math.exp(-(y3 * w35 + y4 * w45 - t5)))
-        """
-        calculating error
-        """
-        error = XOR[i][2] - y5
+            """
+                calculating partial error and change in weight for input and hidden perceptron
+            """
 
-        """
-        calculating partial error and change in weight for output and hidden perceptron
-        """
-        del5 = y5 * (1 - y5) * error
-        dw35 = alpha * y3 * del5
-        dw45 = alpha * y4 * del5
-        dt5 = alpha * (-1) * del5
+            del3 = y3 * (1 - y3) * del5 * w35
+            del4 = y4 * (1 - y4) * del5 * w45
+            dw13 = alpha * XOR[i][0] * del3
+            dw23 = alpha * XOR[i][1] * del3
+            dt3 = alpha * (-1) * del3
+            dw14 = alpha * XOR[i][0] * del4
+            dw24 = alpha * XOR[i][1] * del4
+            dt4 = alpha * (-1) * del4
+            """
+            calculating weight and bias update
+            """
+            w13 = w13 + dw13
+            w14 = w14 + dw14
+            w23 = w23 + dw23
+            w24 = w24 + dw24
+            w35 = w35 + dw35
+            w45 = w45 + dw45
+            t3 = t3 + dt3
+            t4 = t4 + dt4
+            t5 = t5 + dt5
 
-        """
-            calculating partial error and change in weight for input and hidden perceptron
-        """
+            """
+            Since y5 will be in float number between (0 - 1)
+            Here we have used 0.5 as threshold, if output is above 0.5 then class will be 1 else 0
+            """
+            if y5 < 0.5:
+                class_ = 0
+            else:
+                class_ = 1
+            """
+            uncomment below line to see predicted and actual output
+            """
+            # print ("Predicted",class_," actual ",XOR[i][2])
+            """
+            calculating squared error
+            """
+            squaredError = squaredError + (error * error)
+            if squaredError < 0.001:
+                # if error is below   0.001, terminate training (premature termination)
+                break
 
-        del3 = y3 * (1 - y3) * del5 * w35
-        del4 = y4 * (1 - y4) * del5 * w45
-        dw13 = alpha * XOR[i][0] * del3
-        dw23 = alpha * XOR[i][1] * del3
-        dt3 = alpha * (-1) * del3
-        dw14 = alpha * XOR[i][0] * del4
-        dw24 = alpha * XOR[i][1] * del4
-        dt4 = alpha * (-1) * del4
-        """
-        calculating weight and bias update
-        """
-        w13 = w13 + dw13
-        w14 = w14 + dw14
-        w23 = w23 + dw23
-        w24 = w24 + dw24
-        w35 = w35 + dw35
-        w45 = w45 + dw45
-        t3 = t3 + dt3
-        t4 = t4 + dt4
-        t5 = t5 + dt5
-
-        """
-        Since y5 will be in float number between (0 - 1)
-        Here we have used 0.5 as threshold, if output is above 0.5 then class will be 1 else 0
-        """
-        if y5 < 0.5:
-            class_ = 0
-        else:
-            class_ = 1
-        """
-        uncomment below line to see predicted and actual output
-        """
-        # print ("Predicted",class_," actual ",XOR[i][2])
-        """
-        calculating squared error
-        """
-        squaredError = squaredError + (error * error)
-        if squaredError < 0.001:
-            # if error is below   0.001, terminate training (premature termination)
-            break
+Main()
